a802e11b07bcf2ffd2e1a67e8039a406f1d549a1
hp77 <u19ee064@eed.svnit.ac.in>
2021-03-28 12:22:23 +0530

    Tried manim to learn more about visualisation

---
+++ MakerSpace2021/project1/scene.py
@@ -0,0 +1,9 @@
+from manim import *
+
+class SquareToCircle(Scene):
+    
+    def construct(self):
+        circle = Circle()
+        circle.set_fill(PINK, opacity=0.5)
+        self.play(ShowCreation(circle))
+
